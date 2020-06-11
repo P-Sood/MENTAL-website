93629937047f7ba608004d08f0081667e7fde0f7
Pranav <prsood@sfu.ca>
2020-06-10 18:17:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write(index.html);
+  response.write("Hello bitches");
   response.end();
 })
 .listen( process.env.PORT || 8888); 
